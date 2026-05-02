62e7a3c8e118c3d46ec1ead912a330d4eef20b88
noatdk <toanduong8199@gmail.com>
2026-05-02 13:19:58 +0700

    refactor(authors): extract shared author-list-item component
    Reconcile the followed-authors and user-list views: both now render the
    same ion-item layout (avatar + name + optional usertitle/story/follower
    counts + optional follow toggle). Drops virtualScroll on following in
    favor of ngFor since custom components don't fit virtualScroll's height
    assumptions.

--- src/pages/user-list/user-list.module.ts
+++ src/pages/user-list/user-list.module.ts
@@ -3,9 +3,10 @@ import { IonicPageModule } from 'ionic-angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { UserListPage } from './user-list';
+import { AuthorListItemModule } from '../../parts/author-list-item/author-list-item.module';
 
 @NgModule({
   declarations: [UserListPage],
-  imports: [IonicPageModule.forChild(UserListPage), TranslateModule.forChild()],
+  imports: [IonicPageModule.forChild(UserListPage), TranslateModule.forChild(), AuthorListItemModule],
 })
 export class UserListPageModule {}
